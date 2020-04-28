8dfbe8e8132138552993b6d86b5c15e686ba1d12
mgoetzke <mgoetzke@gmail.com>
2020-04-28 16:45:47 -0400

    feat: add tailwind

---
+++ app/components/frame.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import { get, set, computed } from '@ember/object';
+import { sort, notEmpty, equal } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  
+});
