943335397f686bb32d3343c554387e754019bde0
mgoetzke <mgoetzke@gmail.com>
2020-04-29 14:06:16 -0400

    test: update test to check for swatch size

--- tests/integration/components/swatch-test.js
+++ tests/integration/components/swatch-test.js
@@ -9,8 +9,16 @@ module('Integration | Component | swatch', function(hooks) {
   test('it renders the correct color', async function(assert) {
     await render(hbs`<Swatch 
       @shade="red"
+      @size="s"
       />`);
+    assert.dom('[data-test-color]').hasAttribute("style","background-color: red; width:10px; height:10px", 'swatch shows color it is passed');
+  });
 
-      assert.dom('[data-test-color]').hasAttribute('style',"background-color: red", 'swatch shows color it is passed');
+  test('it renders the correct size', async function(assert) {
+    await render(hbs`<Swatch 
+      @shade="black"
+      @size="l"
+      />`);
+    assert.dom('[data-test-size]').hasAttribute('style',"background-color: black; width:30px; height:30px", 'swatch is passed size');
   });
 });
