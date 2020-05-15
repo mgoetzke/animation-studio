68fa4c1d41d59bf054cc3f4e2816aac9f9d3c537
mgoetzke <mgoetzke@gmail.com>
2020-05-15 16:22:57 -0400

    tests: update test with proper element selector

--- tests/integration/components/palette-test.js
+++ tests/integration/components/palette-test.js
@@ -8,23 +8,21 @@ module('Integration | Component | palette', function(hooks) {
 
 
   test('it renders a range of colors', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
     this.set('colorOptions', ['black', 'white', 'red', 'blue', 'green', 'yellow', 'orange']);
     await render(hbs`<Palette 
-                      @colorOptions={{this.colorOptions}}>
-                      hi
-                      </Palette>`);
-    assert.equal(this.colorOptions.length, 17, 'shows possible shades');
+                @colorOptions={{this.colorOptions}}>
+                </Palette>`);
+    assert.dom('[data-test-swatch]').exists({ count: this.colorOptions.length });
   });
-
+  
   test('user can select from colors', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Palette />`);
-    assert.equal(this.element.selectedColor.name, 'black', 'black is default selected');
-    await click('[data-test-color-red]');
-    assert.equal(this.element.selectedColor.name, 'red', 'user can switch colors');
+    this.set('colorOptions', ['black', 'white', 'red', 'blue', 'green', 'yellow', 'orange']);
+    await render(hbs`<Palette 
+                @colorOptions={{this.colorOptions}}>
+                </Palette>`);
+                
+    assert.dom('[data-test-swatch-selected] [data-test-color]').hasStyle({'background-color': 'rgb(0, 0, 0)' } ,'black is default selected');
+    await click('[data-test-swatch=red]');
+    assert.dom('[data-test-swatch-selected] [data-test-color]').hasStyle({'background-color': 'rgb(255, 0, 0)' } ,'user can switch colors');
   });
 });
