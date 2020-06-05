d3cf603b2422b932ad9abedc512d83d443b14837
mgoetzke <mgoetzke@gmail.com>
2020-06-05 17:22:34 -0400

    tests: update frame tests for rendering and selection

--- tests/integration/components/frame-test.js
+++ tests/integration/components/frame-test.js
@@ -1,35 +1,26 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | frame', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders the correct frame', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders the frame of pixels', async function(assert) {
+    const samplePixels = ['red','green', 'blue', 'cyan','yellow','magenta','black','white', 'grey'];
+    this.set('samplePixels', samplePixels)
+    await render(hbs`<Frame @pixels={{samplePixels}}/>`);
 
+    assert.dom('[data-test-frame-row]').exists({count: Math.sqrt(samplePixels.length)});
+    assert.dom('[data-test-pixel]').exists({count: 9});
+  });
+
+  test('The user can edit frame pixels', async function(assert) {
     await render(hbs`<Frame />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Frame>
-        template block text
-      </Frame>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('[data-test-pixel=0]').hasStyle({'background-color': 'rgb(0, 0, 0)' } ,'white is default color');
+    await click('[data-test-pixel=0]');
+    assert.dom('[data-test-pixel=0]').hasStyle({'background-color': 'rgb(255, 0, 0)' } ,'updates color');
   });
 
-  test('The user can edit the frame', async function(assert) {
-
-  });
-
-  test('The user can discard changes or save', async function(assert) {
-
-  });
 
 });
