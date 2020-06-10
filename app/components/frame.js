68d0025abaad5571a15a4f7f6530cf25f2f41329
mgoetzke <mgoetzke@gmail.com>
2020-06-10 10:16:48 -0400

    feat: user can update pixels by accessing at index

--- app/components/frame.js
+++ app/components/frame.js
@@ -1,12 +1,13 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { action, set, computed } from '@ember/object';
 export default class FrameComponent extends Component {
     tagName= ''
 
-    // we'll have frame information saved as an array of colors (for now strings, future hex)
-    // ex. ['red','red','red','red','red','red','red','red','red]
     @computed('pixels')  
     get pixelRows() {
+        const indexedPixels = this.pixels.map((pixel, index) => { 
+            return {'color': pixel, index}
+        })
         const totalPixels = this.pixels.length;
         const rowSize = Math.sqrt(totalPixels);
         const rows = [];
@@ -14,11 +15,18 @@ export default class FrameComponent extends Component {
         while (rows.length*rowSize < totalPixels) {
             const startIndex = rows.length*rowSize;
             const endIndex = startIndex + rowSize;
-            const pixelSlice = this.pixels.slice(startIndex, endIndex);
+            const pixelSlice = indexedPixels.slice(startIndex, endIndex);
             rows.push(pixelSlice);
         }
-        
         return rows;      
+    }
+
+    @action
+    updatePixelColor(pixelIndex) {
+        const newColor = this.paletteColor;
+        const newPixels = [...this.pixels];
+        newPixels[pixelIndex] = newColor;
+        set(this, 'pixels', newPixels);
     }
 }
    
