fca47a8a00d089066dc8b786972db837e0180c29
mgoetzke <mgoetzke@gmail.com>
2020-08-21 16:32:54 -0400

    tests: check basic rendering

---
+++ tests/integration/components/studio-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | studio', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.set('samplePixels', ['red','green', 'blue', 'cyan','yellow','magenta','black','white', 'grey']);
+    this.set('colorOptions', ['black', 'white', 'red', 'blue', 'green', 'yellow', 'orange']);
+
+    await render(hbs`<Studio @pixels={{this.samplePixels}} @colorOptions={{this.colorOptions}}/>`);
+
+    assert.dom('[data-test-animation-frame]').exists('it shows the frame');
+    assert.dom('[data-test-animation-palette]').exists('it shows a basic palette');
+  });
+
+  test('it allows interaction between palette and frame', async function(assert) {
+    // this.set('samplePixels', ['red','green', 'blue', 'cyan','yellow','magenta','black','white', 'grey']);
+    // this.set('colorOptions', ['black', 'white', 'red', 'blue', 'green', 'yellow', 'orange']);
+
+    // await render(hbs`<Studio @pixels={{this.samplePixels}} @colorOptions={{this.colorOptions}}/>`);
+
+    // //click on pixel and it changes color
+    // await click('[]')
+  });
+});
